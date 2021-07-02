59ca67b0cb6b5e53c74edf169455fa9a46b32b02
CookiesDesign <contactocookiesdesign@gmail.com>
2021-07-02 18:47:45 -0500

    Debugging options

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+/* Definicion de variables a utilizar */
 var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
 
 var app = express();
+
+app.use(morgan('combined'));
 
 app.get('/', function(request, response){
     response.send("Hello CookiesDesign was here")
 })
 
 app.listen(3000, function(){
-    console.log('Listening on port 3000');
+    console.log('Listening on port ' + chalk.green('3000'));
 });
